930c8148b81b1613453aef7d46f237a905f9bd72
chailana <chailana35217793@gmail.com>
2024-09-12 21:54:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # Start the megadl command in the background
 /app/venv/bin/python3 -m megadl &
 
-# Start the app
-/app/venv/bin/python3 /app/app.py
+# Start the app on port 8000 (or the required port)
+gunicorn --bind 0.0.0.0:8000 app:app
