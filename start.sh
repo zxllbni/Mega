63e09220adf1a805f2fedf70420c7a0e35fb48fb
chailana <chailana35217793@gmail.com>
2024-09-12 22:05:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start the megadl command in the background
-/venv/bin/python3 -m megadl &
+/app/venv/bin/python3 -m megadl &
 
 # Start the app using gunicorn
 gunicorn --bind 0.0.0.0:8000 app:app
