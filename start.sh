516659420b8c93dd47813d07e5937aee4b441236
chailana <chailana35217793@gmail.com>
2024-09-13 16:14:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start the megadl command in the background
-/app/venv/bin/python3 -m megadl &
+python3 -m megadl &
 
 # Start the app
-/app/venv/bin/python3 /app/app.py
+python3 app.py
